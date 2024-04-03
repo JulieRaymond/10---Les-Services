0ee7f70e7252e245947afef46f294e5bd87d455d
Julie <raymondjulie@proton.me>
2024-04-03 14:31:09 +0200

    quest done + CSS

--- src/app/cocktail-list/cocktail-list.component.ts
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CocktailService} from "../service/cocktail.service";
+import {Cocktail} from '../models/Cocktail.model';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-cocktail-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './cocktail-list.component.html',
   styleUrl: './cocktail-list.component.scss'
 })
 export class CocktailListComponent {
+  cocktails: Cocktail[] = [];
 
+  constructor(private cocktailService: CocktailService) {
+  }
+
+  ngOnInit(): void {
+    this.getCocktails();
+  }
+
+  getCocktails(): void {
+    this.cocktails = this.cocktailService.getCocktails();
+  }
 }
--- src/app/service/cocktail.service.ts
+++ src/app/service/cocktail.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Cocktail} from "../models/Cocktail.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CocktailService {
+  constructor() {
+  }
 
-  constructor() { }
+  getCocktails(): Cocktail[] {
+    return [
+      {name: 'Mojito', price: 8.5, image: 'mojito.jpeg'},
+      {name: 'Cosmopolitan', price: 9.0, image: 'cosmopolitan.jpeg'},
+      {name: 'Pina Colada', price: 7.5, image: 'pinaColada.jpeg'},
+      {name: 'Bloody Mary', price: 8.5, image: 'bloodyMary.jpeg'},
+    ];
+  }
 }
